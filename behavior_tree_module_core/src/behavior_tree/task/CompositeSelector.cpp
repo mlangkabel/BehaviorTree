0b082019ef4d5b4aff4f44f7f8220d075def74c1
mlangkabel <malte.langkabel@gmail.com>
2017-06-24 12:35:20 +0200

    clean iteration of composite tasks

--- behavior_tree_module_core/src/behavior_tree/task/CompositeSelector.cpp
+++ behavior_tree_module_core/src/behavior_tree/task/CompositeSelector.cpp
@@ -11,9 +11,9 @@ CompositeSelector::~CompositeSelector()
 Task::StatusType CompositeSelector::evaluate(std::shared_ptr<Blackboard> blackboard)
 {
 	StatusType status = STATUS_FAILURE;
-	for (size_t i = 0; i < m_children.size(); i++)
+	for (std::shared_ptr<Task> child : m_children)
 	{
-		status = m_children[i]->evaluate(blackboard);
+		status = child->evaluate(blackboard);
 		if (status == STATUS_SUCCESS)
 		{
 			break;
--- behavior_tree_module_core/src/behavior_tree/task/CompositeSequence.cpp
+++ behavior_tree_module_core/src/behavior_tree/task/CompositeSequence.cpp
@@ -11,9 +11,9 @@ CompositeSequence::~CompositeSequence()
 Task::StatusType CompositeSequence::evaluate(std::shared_ptr<Blackboard> blackboard)
 {
 	StatusType status = STATUS_SUCCESS;
-	for (size_t i = 0; i < m_children.size(); i++)
+	for (std::shared_ptr<Task> child : m_children)
 	{
-		status = m_children[i]->evaluate(blackboard);
+		status = child->evaluate(blackboard);
 		if (status == STATUS_FAILURE)
 		{
 			break;
